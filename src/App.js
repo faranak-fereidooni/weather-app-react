36eec48789c2b121a408d343188e51a281766647
Faranak Fereidouni <faranak.fereidooni@gmail.com>
2022-03-10 14:51:31 +0100

    add searchForm component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React, { Component } from 'react';
-import SearchForm from './components/searchForm';
-import './App.css';
+import React, { Component } from "react";
+import SearchForm from "./components/searchForm";
+import { getCity } from "./services/cityService";
+import CityContainer from "./components/cityContainer";
+import "./App.css";
 
 class App extends Component {
-  render() { 
+  state={
+    data:[]
+  }
+  
+  handleCitySubmit = async (e) => {
+    e.preventDefault();
+    const cityInfo = await getCity(e.target.city.value);
+    const data = [];
+    cityInfo.data.map((city) => {
+      data.push({
+        EnglishName: city.EnglishName,
+        LocalizedName: city.AdministrativeArea.LocalizedName,
+        CountryID: city.AdministrativeArea.CountryID,
+        Key: city.Key,
+      });
+    });
+    this.setState({data});
+  };
+
+  render() {
     return (
       <div className="container">
         <h1 className="text-light m-5">Weather App</h1>
-      <SearchForm/>
+        <SearchForm onSubmit={this.handleCitySubmit} />
+        <CityContainer cityInfo={this.state.data} />
       </div>
     );
   }
 }
- 
+
 export default App;
--- src/components/searchForm.jsx
+++ src/components/searchForm.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
 const SearchForm = (props) => {
+  const { onSubmit } = props;
   return (
-    <form className="change-location text-center text-white my-4">
+    <form
+      className="change-location text-center text-white my-4"
+      onSubmit={onSubmit}
+    >
       <label htmlFor="city">Enter a location for weather information</label>
       <input type="text" name="city" className="form-control p-4 my-2" />
     </form>
